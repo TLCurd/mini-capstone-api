67ea5045cef58aba4f8bb39ab01dabc7933ebcc5
TLCurd <TLCurdChiro@gmail.com>
2022-03-09 11:18:54 -0700

    adds 3 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.create(name: "PUMA FUTURE Z 1.2 FG/AG Soccer Cleats", price: 199.99, image_url: "https://www.imagehandler.net/preview/?istyle=0000&fmt=jpg&w=370&h=370&cmp=100&c=999&img=A1061470000&iset=0100&iindex=0088&retBlank=1x1&bg=f6f6f6", description: "Lock in. Drive them crazy. The PUMA FUTURE Z was developed with one goal: totally re-engineer a soccer cleat to enhance the playing style of the world’s most dynamic playmaker – Neymar Jr. Built around an adaptive FUZIONFIT+ compression band, the boot provides optimal lock-in and support for movement so explosive it will drive the opposition crazy.")

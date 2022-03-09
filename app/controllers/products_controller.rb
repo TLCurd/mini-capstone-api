4f5be7382dd9cce0de8a2e6d080c55bd855b8ccc
TLCurd <TLCurdChiro@gmail.com>
2022-03-09 13:08:18 -0700

    adds all routes and methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,16 @@ class ProductsController < ApplicationController
     render json: product.as_json
   end
 
+  def mercurial
+    product = Product.second.as_json
+    render json: product
+  end
+
+  def puma
+    product = Product.third.as_json
+    render json: product
+  end
+
   def all
     product = Product.all.as_json
     render json: product
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   
   get "/predator" => "products#predator"
   get "/all" =>"products#all"
+  get "/mercurial" => "products#mercurial"
+  get "/puma" => "products#puma"
 end
   

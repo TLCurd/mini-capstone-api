609f9cf9777d65d758523e025687d8b5c82c044f
TLCurd <TLCurdChiro@gmail.com>
2022-03-18 12:09:22 -0600

    adds jb gem colors

--- app/models/product.rb
+++ app/models/product.rb
@@ -39,5 +39,5 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0}
   validates :description, presence: true
-  validates :description, length: { in: 10..500}
+  validates :description, length: { in: 10..800}
 end

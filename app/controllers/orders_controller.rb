d4bc58ddeddd73c3998e11d76ca31bca3c7b0847
TLCurd <TLCurdChiro@gmail.com>
2022-03-23 11:51:06 -0600

    adds authenticate user to orders controller; adds authenticate admin to products controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user, only: [:view, :create]
   def index
     if current_user
       orders = Order.where(user_id: current_user.id)
@@ -10,23 +11,31 @@ class OrdersController < ApplicationController
   
   
   def create
-    product = Product.find_by(id: params[:product_id])
-    tax = (product.price * params[:quantity]) * 0.08
-    total = (product.price * params[:quantity]) + tax
-    order = Order.new(user_id: current_user.id, product_id: params[:product_id], quantity: params[:quantity], subtotal: product.price * params[:quantity], tax: tax, total: total)
-    if order.save
-      render json: order.as_json   
+    if current_user
+      product = Product.find_by(id: params[:product_id])
+      tax = (product.price * params[:quantity].to_i) * 0.09
+      total = (product.price * params[:quantity].to_i) + tax
+      order = Order.new(user_id: current_user.id, product_id: params[:product_id], quantity: params[:quantity], subtotal: product.price * params[:quantity].to_i, tax: tax, total: total)
+      # could also use the tax and total methods from product class:
+      # order = Order.new(user_id: current_user.id, product_id: params[:product_id], quantity: params[:quantity], subtotal: product.price * params[:quantity], tax: product.tax * params[:quantity], total: product.total * params[:quantity])
+      if order.save
+        render json: order.as_json   
+      else
+        render json: { message: "This order was not created due to: #{order.errors.full_messages}."}
+      end
     else
-      render json: { message: "This order was not created due to: #{order.errors.full_messages}."}
-    end     
+      render json: { message: "You must be logged in to dot that."}, status: :unauthorized 
+    end    
   end
 
   def show
-    if current_user
-      order = Order.find_by(id: params[:id])
+    order = Order.find_by(id: params[:id])
+    if current_user == nil 
+      render json: {message: "You must be logged in to view your order."}, status: :unauthorized
+    elsif order.user_id == current_user.id
       render json: order.as_json  
     else
-      render json: {message: "You must be logged in to view your orders."}
+      render json: {message: "You must be logged in to view your order."}, status: :unauthorized
     end
   end
 end
